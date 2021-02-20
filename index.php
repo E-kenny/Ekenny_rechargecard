18768421c43265dfe12a45376aa03a37c90452b1
Ekene <ekennyobiasogu@gmail.com>
2021-02-20 15:37:08 +0100

    airtime generator

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
 //This code prints out 200 twelve digits git 
-for ($i = 0; $i < 200; $i++){
-    echo rand(100000000000,999999999999).'<br>';
+
+function airtimeGenerator(){
+    $cards = [];
+    $counter = 0;
+    while ($counter < 200){
+        $pin =  rand(100000000000,999999999999);
+        for ($i=0; $i <= count($cards); $i++) { 
+
+            if (!in_array($pin,$cards)) {
+
+                $cards[$counter] = $pin;
+                $counter++;
+            } 
+            
+        }
+
+    }
+
+    for ($j=0; $j < count($cards); $j++) { 
+        echo $cards[$j] . "<br>";
+    }
 }
+airtimeGenerator();
 ?>
